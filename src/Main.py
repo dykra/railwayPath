dbd9ae56cdfb02a7975f5f0e62f7baaad9739fe3
McDusia <magdalno222@gmail.com>
2018-09-03 08:10:18 +0200

    cleanups, nice, small error, new seed, plots

--- src/Main.py
+++ src/Main.py
@@ -2,19 +2,12 @@ import sys
 import pyodbc
 import pandas as pd
 import tensorflow as tf
-from keras.losses import mean_squared_error, mean_absolute_percentage_error
+from keras.losses import mean_squared_error
 from keras.models import Sequential
 from keras.layers import Dense, K
 import numpy
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-import matplotlib.pyplot as plt
+from matplotlib import pyplot
 import logging
-
-from keras.wrappers.scikit_learn import KerasRegressor
 
 
 """
@@ -25,7 +18,7 @@ from keras.wrappers.scikit_learn import KerasRegressor
     the initialization method (second arg) as init and specified
     the activation function using the activation argument.       
     
-:return: Model
+:returns: Model
     
 """
 
@@ -34,40 +27,11 @@ def baseline_model():
     model = Sequential()
     model.add(Dense(70, input_dim=70, kernel_initializer='normal', activation='relu'))
     model.add(Dense(1, kernel_initializer='normal'))
-    # model.compile(loss='mean_squared_error', optimizer='adam')
-    precision = as_keras_metric(tf.metrics.precision)
-    recall = as_keras_metric(tf.metrics.recall)
-    model.compile(loss=mean_absolute_percentage_error, optimizer='adam', metrics=[precision, recall])
+    model.compile(loss=mean_squared_error, optimizer='adam',
+                  metrics=['mean_squared_error',
+                           'mean_absolute_error', 'mean_absolute_percentage_error',
+                           'cosine_proximity'])
     return model
-
-
-def soft_acc(y_true, y_pred):
-    return K.mean(K.equal(K.round(y_true), K.round(y_pred)))
-
-
-"""
-    Function to wrap tensorflow metrics.
-
-:return: Wrapper
-
-"""
-
-
-def as_keras_metric(method):
-    import functools
-    from keras import backend as K
-
-    """
-            Wrapper for turning tensorflow metrics into keras metrics
-    """
-    @functools.wraps(method)
-    def wrapper(self, args, **kwargs):
-        value, update_op = method(self, args, **kwargs)
-        K.get_session().run(tf.local_variables_initializer())
-        with tf.control_dependencies([update_op]):
-            value = tf.identity(value)
-        return value
-    return wrapper
 
 
 def main():
@@ -124,37 +88,30 @@ def main():
     print(y)
 
     #  ----------  Fix random seed for reproducibility
-    seed = 2 ** 32 - 29638824
+    seed = 7
     numpy.random.seed(seed)
 
-    #  ----------  Evaluate model with standardized dataset
-    estimator = KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=1, verbose=False)
-
-    kfold = KFold(n_splits=2, random_state=seed)
-
-    results = cross_val_score(estimator, x.values, y.values, cv=kfold, n_jobs=1, verbose=False)
-
+    model = baseline_model()
+    results = model.fit(x.values, y.values, epochs=9265, batch_size=len(x.values), verbose=2)
+    logging.info('--= Plot metrics =--')
+    pyplot.plot(results.history['mean_squared_error'])
+    pyplot.show()
+    pyplot.plot(results.history['mean_absolute_error'])
+    pyplot.show()
+    pyplot.plot(results.history['mean_absolute_percentage_error'])
+    pyplot.show()
+    pyplot.plot(results.history['cosine_proximity'])
+    pyplot.show()
     logging.info('--= Model counted. =--')
-    print("Results: %.2f (+/- %.2f) MSE" % (results.mean(), results.std() * 2))
 
-    estimator.fit(x.values, y.values, verbose=False)
-
-    prediction = estimator.predict(x.values)
+    prediction = model.predict(x.values)
 
     print('First prediction:', prediction[0])
 
-    # print("{0:.15f}".format(mean_squared_error(prediction, Y.values)))
-    print('mean_squared_error:', abs(mean_squared_error(prediction, y.values)))
-    print('estimator.model.loss:', abs(estimator.model.loss(prediction, y.values)))
-
     logging.info('--= Model summary: =--')
-    estimator.model.summary()
+    model.summary()
 
-    scores = estimator.model.evaluate(x.values, y.values)
-
-    print("Accuracy: %.2f%%" % scores)
-
-    estimator.model.save('model.h5')
+    model.save('model.h5')
     logging.info('--= Model saved in model.h5 file. =--')
 
 
