c0a00053eab2ae7463d870de1c858d61b6ba11a1
dykra <asiapalewicz@gmail.com>
2018-09-11 10:18:24 +0200

    JP_joining_script add logger

--- src/PriceEstimatorComponent/Main.py
+++ src/PriceEstimatorComponent/Main.py
@@ -1,11 +1,23 @@
 import argparse
+import logging
+
+from src.PriceEstimatorComponent.Utils.logger import create_loggers_helper
+
+
+def create_logger():
+    _logger = logging.getLogger(__name__)
+    _logger.setLevel(logging.DEBUG)
+    return create_loggers_helper(_logger)
+
+
+logger = create_logger()
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Program to predict lands prices.')
     parser.add_argument('--database', help='Database name.')
     parser.add_argument('--server', help='Server address.')
-    parser.add_argument('--du', help='Database user name.')
+    parser.add_argument('--database_user', help='Database user name.')
     parser.add_argument('--connection_string', help='Connection string')
     parser.add_argument('--save', help='Save counted values into the database.',
                         action='store_true')
@@ -14,11 +26,11 @@ def parse_args():
 
 def main():
     args = parse_args()
-    if args.connection_string is None\
-            and (args.database is None or args.server is None or args.du is None):
-        print('ERROR')
-    print(args)
-
+    # Not ending the program because model may be serialized
+    if args.connection_string is None \
+            and (args.database is None or args.server is None or args.database_user is None):
+        logger.warning('Unable to connect to database.')
+        
 
 if __name__ == '__main__':
     main()
