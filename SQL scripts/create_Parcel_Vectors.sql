ea83365fd50c63034f419f84e3151c73f2106309
dykra <asiapalewicz@gmail.com>
2018-10-12 17:25:50 +0200

    scripts update comments

--- SQL scripts/create_Parcel_Vectors.sql
+++ SQL scripts/create_Parcel_Vectors.sql
@@ -1,4 +1,10 @@
 ﻿-- =============================================
+-- Author:		Magdalena Nowak, Joanna Palewicz
+-- Create date: 01.05.2018
+-- Description:	Preparation of the table Parcel_Vectors
+-- =============================================
+
+-- =============================================
 -- CREATE NEW TABLE PARCEL_VECTORS
 -- not interested values are dropped
 -- =============================================
