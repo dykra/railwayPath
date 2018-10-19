629370f92dfdd51a3d512b3e60d64ef0ace618f7
Joanna Palewicz <asiapalewicz@gmail.com>
2018-10-19 14:32:08 +0200

    Scripts Add indexes

--- SQL scripts/create_Parcel_Vectors.sql
+++ SQL scripts/create_Parcel_Vectors.sql
@@ -42,6 +42,35 @@ SELECT
 INTO PARCEL_VECTORS FROM PARCEL;
 
 -- =============================================
+-- Add indexes to PARCEL_VECTORS
+-- Primary Key on objectID
+-- Spatial index on Shape
+-- =============================================
+
+ALTER TABLE PARCEL_VECTORS
+ADD CONSTRAINT PK_ObjectID PRIMARY KEY CLUSTERED (OBJECTID);  
+GO  
+
+SET ARITHABORT ON
+SET CONCAT_NULL_YIELDS_NULL ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+SET NUMERIC_ROUNDABORT OFF
+GO
+
+CREATE SPATIAL INDEX [SP_Shape] ON [dbo].[PARCEL_VECTORS]
+(
+	[Shape]
+)USING  GEOMETRY_AUTO_GRID 
+WITH (BOUNDING_BOX =(6275487.79, 1384146.14, 6668480.77, 2122084.6), 
+CELLS_PER_OBJECT = 16, PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
+
+
+
+-- =============================================
 -- Get data from Zoning_Code column
 -- Adding and filling new column Simple_Zoning_Code
 -- Adding and filling new column City
